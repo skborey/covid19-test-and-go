982ef9bb821f03f0e067550bef5940f8787a989b
skborey <skborey@gmail.com>
2022-04-06 12:17:37 +0700

    get all ids

---
+++ index.js
@@ -0,0 +1,63 @@
+const fetch = require("cross-fetch");
+
+async function getHtml(url) {
+
+  // const response = await fetch("https://web.thailandsha.com/shaextraplus?province=10&page="+page)
+// .then(response => response.text())
+// .then(html => {
+//   console.log(getDetail(html));
+// })
+// .catch((error) => {
+//   console.error('Error:', error);
+// });
+// }
+
+  const response = await fetch(url);
+
+  return response.text();
+
+}
+
+function getIdsFromText(text = "") {
+  //https://web.thailandsha.com/shalists/detail/65177
+  const regexId = /(?<=https:\/\/web.thailandsha.com\/shalists\/detail\/)(.*)(?="\>)/g;
+  
+  const results = text.match(regexId)
+  
+  return results;
+}
+
+
+async function getIdsOfPage(page) {
+  const html = await getHtml("https://web.thailandsha.com/shaextraplus?province=10&page="+page)
+
+  // console.log(html);
+
+  return getIdsFromText(html);
+}
+
+
+async function worker() {
+
+  let hotelIds = [];
+
+  const tasks = [];
+  
+  for (let page = 1; page <=43; page++) {
+
+    tasks.push(new Promise(async(r, e) => {
+      const ids = await getIdsOfPage(page);
+
+      r(ids)
+    }))
+  }
+
+  Promise.all(tasks).then((values) => {
+    for (let ids of values) {
+      hotelIds = hotelIds.concat(ids);
+    }
+    console.log(hotelIds)
+  });
+}
+
+worker();
